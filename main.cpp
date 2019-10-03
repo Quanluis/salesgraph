82197e5946029e934f0945fb437188cfd3ec098b
Quanluis <quanluis@protonmail.com>
2019-10-03 11:16:36 -0400

    Created a if statement that checks to see if file is found and valid then nested my other actions inside it.

--- main.cpp
+++ main.cpp
@@ -5,18 +5,25 @@ using namespace std;
 
 int main() {
 
-    ifstream file ("C:\\Users\\Luis\\Desktop\\projects\\salesgraph\\sales.txt");
+    ifstream file ("sales.txt");
 
     std::vector<std::string> names;
 
     string input;
 
-    while(file >> input){
+    if(!file){
+        cout << "File is not found.";
+    } else {
+        while(file >> input)
+        {
         names.push_back(input);
-    }
-    for(string name : names){
+        }
+    for(string name : names)
+        {
         cout << name << endl;
+        }
     }
+
 
    system("pause");
 
