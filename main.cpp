15ffa7b50a761183f4ada829506a739671b8066a
Quanluis <quanluis@protonmail.com>
2019-10-04 18:05:15 -0400

    Added comments and round function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
+//Luis Garzon
+//COP2000.0M2
+//Program takes file info of sales then displays a certain number of stars based on thousand sales
+//rounded to the nearest 1000
 #include <iostream>
 #include <fstream>
+#include <cmath>
 using namespace std; 
 
 int main() {
@@ -7,32 +12,55 @@ int main() {
     ifstream inputFile;
     string name;
 
+    //Opens text files
+
     inputFile.open("sales.txt");
+
+    //Program info
 
     cout << "Today's Sales Chart" << endl << endl;
     cout << "Gathering input from file..." << endl << endl;
     cout << "Daily Sales (each * = $1000)" << endl << endl;
+
+    //If file is not found run this cout statement
+    //Else run program
         
     if(!inputFile){
         cout << "File not found!!!" << endl;
 
     } else {
+
+        //for loop executes until conditions are met. 
+
         for(int count = 1; count <=3; count++ ){
+
+        // Variables
     
-        int num1, result;
+        double num1, result;
 
         inputFile >> name;
 
+        //convert string to int, store in variable
+
         num1 = stoi(name);
 
+        //stores results in int variable
+
         result = num1 / 1000;
+
+        // For loop that prints stars until condition it met
 
         for(int star = 1; star <= result; star++ ){
             cout << "*";
         }
-        cout << result << endl;
+        //Display results with number of asterisk
+        //plus numerical count of how are many displayed.
+
+        cout << lround(result) << endl;
      }
     }
+
+    //close file and pause system
 
     inputFile.close();
 
